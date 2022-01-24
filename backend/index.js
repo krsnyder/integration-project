d96dc87e5de69eb12acc43b5e1034f007ece6b81
Kirk Snyder Jr <Kirk.Robert.Snyder@gmail.com>
2022-01-24 12:38:38 -0500

    Adding template literals to server.listen

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 server.listen(port, () => {
-  console.log("Server listening on port " + port);
+  console.log(`Server listening on port ${port}`);
 });
