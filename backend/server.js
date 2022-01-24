b0be4630225939d780c27cfff4ee6efb70c5f442
Kirk Snyder Jr <Kirk.Robert.Snyder@gmail.com>
2022-01-24 12:40:01 -0500

    Adding server functionality to handle file uploads

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/'})
 const server = express();
 
-server.get('posts', (req, res) => {
-  res.status(200).json();
+server.get('/posts', (req, res) => {
+  res.status(200).json({
+    "message": "Success"
+  });
 });
 
-server.post('/posts', (req, res) => {
+server.post('/posts', upload.single('image'), (req, res) => {
+  console.log(req.file)
+  const { description } = req.body;
   res.status(200).json();
 });
 
